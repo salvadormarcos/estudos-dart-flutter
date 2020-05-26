5240e6eb7ac9137b9578713f5f9649d77126a221
Marcos Koch Salvador <marcosks0@gmail.com>
2020-05-25 22:45:44 -0300

    Adicionado comentários

--- dart/4-funcoes/main.dart
+++ dart/4-funcoes/main.dart
@@ -1,7 +1,10 @@
+// A função main() do Dart funciona de forma semelhante a do Java.
+// No entanto, aqui eu recebo uma Lista ao invés de uma Array
 void main(List<String> args) {
   print('Função main() com argumentos invocada!');
   print('Total de argumentos passados: ${args.length}');
 
+  // O Dart (assim como o JS/TS), permite que a Arrow Function modifique variáveis externas
   var idx = 1;
   args.forEach((el) => print('Nº ${idx++}: ${el}'));
 }
